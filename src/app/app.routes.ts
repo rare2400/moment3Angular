67530730b0694c356036879c8d576dfa327b1398
Ramona <rare2400@student.miun.com>
2025-05-04 18:02:51 +0200

    skapar routes till undersidorna

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ConverterComponent } from './converter/converter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'converter', component: ConverterComponent},
+    { path: 'about', component: AboutComponent}
+];
