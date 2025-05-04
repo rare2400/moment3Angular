a353f54d86bb20e251d5ad0980af1a2f1002768f
Ramona <rare2400@student.miun.com>
2025-05-04 18:06:44 +0200

    lägger till route som tar länk utan /text till startsidan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,6 @@ import { ConverterComponent } from './converter/converter.component';
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'converter', component: ConverterComponent},
-    { path: 'about', component: AboutComponent}
+    { path: 'about', component: AboutComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
